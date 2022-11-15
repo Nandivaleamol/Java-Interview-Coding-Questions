ea747c422f31fa9bf3a37164bd6354c1a2ae5a6d
Nandivaleamol <nandivaleamol@gmail.com>
2022-11-15 14:20:18 +0530

    String coding practice

---
+++ src/practice/string/LowerToUpperViceVersa.java
@@ -0,0 +1,22 @@
+package practice.string;
+
+public class LowerToUpperViceVersa {
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer( "Java Programming");
+        convertOpposite(str);
+    }
+
+    public static void convertOpposite(StringBuffer str){
+        int len = str.length();
+
+        for (int i=0; i<len; i++){
+            Character c = str.charAt(i);
+            if (Character.isLowerCase(c)){
+                str.setCharAt(i, Character.toUpperCase(c));
+            }else {
+                str.setCharAt(i, Character.toLowerCase(c));
+            }
+        }
+        System.out.println(str);
+    }
+}
