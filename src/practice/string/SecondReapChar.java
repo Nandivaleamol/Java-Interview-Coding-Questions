2724379f54925b2135af8eae53cc530ad24078ab
Nandivaleamol <nandivaleamol@gmail.com>
2022-11-17 18:41:17 +0530

    String coding practice

---
+++ src/practice/string/SecondReapChar.java
@@ -0,0 +1,32 @@
+package practice.string;
+
+import java.util.*;
+
+public class SecondReapChar {
+    public static void main(String[] args) {
+        String str = "PREPARED";
+        String intitialString = "statistic non";
+        secondReapChar(intitialString);
+//        secondReapChar(str);
+    }
+
+    public static void secondReapChar(String str){
+        StringBuffer stringBuffer = new StringBuffer();
+        int count = 0;
+        char[] ch = str.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = 0; j < ch.length; j++) {
+                if (ch[i] == ch[j]) {
+                    count++;
+                }
+            }
+            if (count > 1) {
+                count = 0;
+            } else
+                stringBuffer.append(ch[i]);
+                //System.out.println(ch[i]);
+        }
+        System.out.println(stringBuffer.charAt(1));
+
+    }
+}
