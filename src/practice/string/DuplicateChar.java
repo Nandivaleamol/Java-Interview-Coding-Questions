3484329bcd227e3591bc21288e1258dbf16f075a
Nandivaleamol <nandivaleamol@gmail.com>
2022-11-17 15:30:19 +0530

    String coding practice

--- src/practice/string/DuplicateChar.java
+++ src/practice/string/DuplicateChar.java
@@ -1,25 +1,23 @@
 package practice.string;
-
 import java.util.Scanner;
 
 public class DuplicateChar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-
         char d = 0;
         int count = 0;
-
-        for (int i=0; i<str.length(); i++){
+        for (int i=0; i<str.strip().length(); i++){
             for (int j=i+1; j<str.length(); j++){
-                if (str.charAt(i) == str.charAt(j)){
-                    if (d!=str.charAt(i)){
-                        count++;
-                        d = str.charAt(i);
-                        System.out.println("Duplicate Character is : "+ d);
-                        break;
+                if (!Character.isWhitespace(str.charAt(j)) & !Character.isWhitespace(str.charAt(i)))
+                    if (str.charAt(i) == str.charAt(j)){
+                        if (d!=str.charAt(i)){
+                            count++;
+                            d = str.charAt(i);
+                            System.out.println("Duplicate Character is : "+ d);
+                            break;
+                        }
                     }
-                }
             }
         }
         System.out.println("Number of duplicate character is: "+count);
