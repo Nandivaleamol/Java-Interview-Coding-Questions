2ed772c34948c590b1b734a8e440fdc64db47d51
Amol Nandivale <nandivaleamol@gmail.com>
2023-01-13 11:58:40 +0530

    String coding practice

--- src/practice/string/StringWordReverse.java
+++ src/practice/string/StringWordReverse.java
@@ -19,22 +19,18 @@ public class StringWordReverse {
 
         System.out.println();
 
-        ////////////////////////////////////////////
-
         // iterating string array using stream api
        Arrays.stream(strArr).distinct().forEach(s->System.out.print(s+" "));
 
 
         // Problem Statement:- Write a java program reverse the given string sentence character wise and remove whitespaces.
-
         char[] strCharArr = str.trim().toCharArray();
         System.out.println();
-       for (int i=strCharArr.length-1; i>=0; i--){
+        for (int i=strCharArr.length-1; i>=0; i--){
            if (!Character.isWhitespace(strCharArr[i])){
                System.out.print(strCharArr[i]+" ");
            }
-       }
+        }
 
-       ////////////////////////////////////////////////////////////
     }
 }
