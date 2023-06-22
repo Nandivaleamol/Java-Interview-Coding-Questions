a70c1780bbf4847fc4ec2e894cec9d7478b15e05
Amol Nandivale <nandivaleamol@gmail.com>
2023-06-22 08:25:25 +0530

    java interview questions

---
+++ src/practice/array/Last2MaxNumbers.java
@@ -0,0 +1,13 @@
+package practice.array;
+
+public class Last2MaxNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,3,5,6,8,7,9};
+
+        for (int i=0; i<=arr.length; i++ ){
+            for (int j=i; j<arr.length; j++){
+
+            }
+        }
+    }
+}
